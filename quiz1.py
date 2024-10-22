77274cab74c9062aef4b68cdb437e3c52b3ca54d
Sergei Barbarash <sgt.sgt@gmail.com>
2024-10-22 19:59:00 +0300

    vasya's solutions

--- quiz1.py
+++ quiz1.py
@@ -1,5 +1,6 @@
 # порядок задач -- от более легкой к более сложным, по моей оценке. если что-нибудь не будет получаться, ничего, обсудим
-# примеры -- в test_quiz1.py
+# примеры -- в bak_test_quiz1.py
+
 
 def two_sum(nums: list[int], target: int) -> list[int]:
     """
@@ -11,17 +12,42 @@ def two_sum(nums: list[int], target: int) -> list[int]:
 
     You can return the answer in any order.
     """
-    raise NotImplementedError("Not implemented yet")
+    for num in range(len(nums)):
+        for n in range(num + 1, len(nums)):
+            if nums[num] + nums[n] == target:
+                return [num, n]
+
+
+def test_two_sum():
+    assert frozenset(two_sum([2, 7, 11, 15], 9)) == frozenset([0, 1])
+    assert frozenset(two_sum([3, 2, 4], 6)) == frozenset([1, 2])
+    assert frozenset(two_sum([3, 3], 6)) == frozenset([0, 1])
+
 
 def is_palindrome(x: int) -> bool:
     """
     leetcode #9
 
     Given an integer x, return true if x is a palindrome, and false otherwise.
-    
+
     задача со звездочкой: решить чисто математически, без превращения числа в строку
     """
-    raise NotImplementedError("Not implemented yet")
+    a = 0
+    c = len(str(x))
+    for num in range(c):
+        num += 1
+        b = x % 10
+        a += b * (10 ** (c - num))
+        x = x // 10
+
+    return a
+
+
+def test_is_palindrome():
+    assert is_palindrome(121)
+    assert not is_palindrome(-121)
+    assert not is_palindrome(10)
+
 
 def longest_common_prefix(strs: list[str]) -> str:
     """
@@ -31,7 +57,25 @@ def longest_common_prefix(strs: list[str]) -> str:
 
     If there is no common prefix, return an empty string "".
     """
-    raise NotImplementedError("Not implemented yet")
+    str = ""
+    equ = False
+    for a in strs:
+        if len(str) < len(a):
+            str = a
+            equ = False
+        elif len(str) == len(a):
+            equ = True
+    if not equ:
+        return str
+    else:
+        return ""
+
+
+def test_longest_common_prefix():
+    assert "fl" == longest_common_prefix(["flower", "flow", "flight"])
+    assert "" == longest_common_prefix(["dog", "racecar", "car"])
+    assert "" == longest_common_prefix([""])
+
 
 def length_of_longest_substring(s: str) -> int:
     """
@@ -40,3 +84,9 @@ def length_of_longest_substring(s: str) -> int:
     Given a string s, find the length of the longest substring without repeating characters.
     """
     raise NotImplementedError("Not implemented yet")
+
+
+def test_length_of_longest_substring(self):
+    assert length_of_longest_substring("abcabcbb") == 3  # "abc"
+    assert length_of_longest_substring("bbbbb") == 1  # "b"
+    assert length_of_longest_substring("pwwkew") == 3  # "wke"
