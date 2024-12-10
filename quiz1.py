00f4fe73209e6618e5b51ff718a7e9ee1510ea2c
vassi <asrtjlbv@ffgh.com>
2024-12-10 17:23:30 +0300

    second type longest_common_prefix

--- quiz1.py
+++ quiz1.py
@@ -156,14 +156,26 @@ def longest_common_prefix(strs: list[str]) -> str:
 
     If there is no common prefix, return an empty string "".
     """
-    strs.sort()
-    first = strs[0]
-    last = strs[-1]
-    minl = min(len(first), len(last))
-    i = 0
-    while i < minl and first[i] == last[i]:
-        i += 1
-    return first[:i]
+    # strs.sort()
+    # first = strs[0]
+    # last = strs[-1]
+    # minl = min(len(first), len(last))
+    # i = 0
+    # while i < minl and first[i] == last[i]:
+    #     i += 1
+    # return first[:i]
+    if not strs:
+        return ""
+    prefix = strs[0]
+
+    for string in strs[1:]:
+        while string[:len(prefix)] != prefix and prefix:
+            prefix = prefix[:-1]
+
+        if not prefix:
+            return ""
+
+    return prefix
 
 
 def test_longest_common_prefix():
